7bb3e9c4b2f6d7b1e4bd578188db03c3fbdc0056
Leo-Noriega <leonoriega100@gmail.com>
2024-07-22 17:03:00 -0600

    feat: Delete directories created
    Added the function that deletes the directories with it's contents of the directories that were created during the process

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ from Database.RetrieveDevices import retrive_devices
 from Database.UnifyJsons import get_unified_jsons
 from Sismedia.RetrieveData import process_devices
 from Sismedia.RetrieveToken import update_token_env
+from Database.GenerateInserts import delete_directories
 
 if __name__ == '__main__':
     update_token_env()
@@ -12,3 +13,4 @@ if __name__ == '__main__':
     get_unified_jsons()
     process_all_json_files()
     insert_data()
+    delete_directories()
